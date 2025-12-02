7f5d6746c8bb82d107df52f359e8c09a5a16fd4c
1234abhibisht <abhibisht129@gmail.com>
2025-12-02 22:58:12 +0530

    Update header.h

--- include/header.h
+++ include/header.h
@@ -1,17 +1,22 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 
-// Function prototypes
-void wordFrequenc(char *freqStr, char frequencyWord[]);
+enum Menu {
+    frequency = 1,
+    find = 2,
+    replace = 3,
+    linechar = 4,
+    end = 5
+};
+
+void wordFrequency(char *freqStr, char frequencyWord[]);
 void wordFind(char findWord[], FILE *f2);
 void replaceWords(char *oldStr, char *wordOld, char *wordNew, char *fileName);
 void lineCharCount(FILE *f4);
 
 #endif
+
 
 
