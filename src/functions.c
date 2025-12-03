f40d6b1305d8d0c511608931917ab5e1b1933fbb
1234abhibisht <abhibisht129@gmail.com>
2025-12-03 08:17:19 +0530

    Update functions.c

--- src/functions.c
+++ src/functions.c
@@ -1,8 +1,10 @@
 #include "header.h"
 
-void wordFrequency(char *freqStr, char frequencyWord[]) {
+void wordFrequency(char *freqStr, char frequencyWord[])
+{
     int frequencyCount = 0;
-    while ((freqStr = strstr(freqStr, frequencyWord)) != NULL) {
+    while ((freqStr = strstr(freqStr, frequencyWord)) != NULL)
+    {
         frequencyCount++;
         freqStr += strlen(frequencyWord);
     }
@@ -10,40 +12,48 @@ void wordFrequency(char *freqStr, char frequencyWord[]) {
     printf("\n%s appears %d times\n\n", frequencyWord, frequencyCount);
 }
 
-void wordFind(char findWord[], FILE *f2) {
+void wordFind(char findWord[], FILE *f2)
+{
     char linebuffer[1000];
     int linecount = 0;
     int found = 0;
 
     printf("\n");
 
-    while (fgets(linebuffer, sizeof(linebuffer), f2)) {
+    while (fgets(linebuffer, sizeof(linebuffer), f2))
+    {
         linecount++;
-        if (strstr(linebuffer, findWord)) {
+        if (strstr(linebuffer, findWord))
+        {
             printf("Word found in line %d\n", linecount);
             found = 1;
         }
     }
 
     if (!found)
+    {
         printf("Word not found in file\n");
+    }
 
     printf("\n");
 }
 
-void replaceWords(char *oldStr, char *wordOld, char *wordNew, char *fileName) {
+void replaceWords(char *oldStr, char *wordOld, char *wordNew, char *fileName)
+{
     int oldWordLen = strlen(wordOld);
     int newWordLen = strlen(wordNew);
     int count = 0;
 
     char *ptr1 = oldStr;
-    while ((ptr1 = strstr(ptr1, wordOld)) != NULL) {
+    while ((ptr1 = strstr(ptr1, wordOld)) != NULL)
+    {
         count++;
         ptr1 += oldWordLen;
     }
 
     char *newStr = (char *)malloc(strlen(oldStr) + (newWordLen - oldWordLen) * count + 1);
-    if (!newStr) {
+    if (!newStr)
+    {
         printf("Failed to allocate memory\n");
         return;
     }
@@ -51,7 +61,8 @@ void replaceWords(char *oldStr, char *wordOld, char *wordNew, char *fileName) {
     char *ptr2 = oldStr;
     char *str2 = newStr;
 
-    while ((ptr1 = strstr(ptr2, wordOld)) != NULL) {
+    while ((ptr1 = strstr(ptr2, wordOld)) != NULL)
+    {
         int gap = ptr1 - ptr2;
         memcpy(str2, ptr2, gap);
         str2 += gap;
@@ -65,7 +76,8 @@ void replaceWords(char *oldStr, char *wordOld, char *wordNew, char *fileName) {
     strcpy(str2, ptr2);
 
     FILE *temp = fopen("temp.txt", "w");
-    if (!temp) {
+    if (!temp)
+    {
         printf("Failed to open temporary file\n");
         free(newStr);
         return;
@@ -80,17 +92,16 @@ void replaceWords(char *oldStr, char *wordOld, char *wordNew, char *fileName) {
     free(newStr);
 }
 
-void lineCharCount(FILE *f4) {
-    int ch, charac = 0, line = 0;
+void lineCharCount(FILE *f4)
+{
+    int ch, charac = 0, line = 1;
 
-    while ((ch = fgetc(f4)) != EOF) {
+    while ((ch = fgetc(f4)) != EOF)
+    {
         charac++;
         if (ch == '\n')
             line++;
     }
-
-    if (line == 0 && charac > 0)
-        line = 1;
 
     printf("Total characters: %d\n\n", charac);
     printf("Total lines: %d\n\n", line);
